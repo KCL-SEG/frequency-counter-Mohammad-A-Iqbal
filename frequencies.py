f619e05ebe774ea8252aea1b16a863dbd9d91650
Mohammad A Iqbal <nea.testing.coursework@gmail.com>
2022-11-28 17:38:16 +0000

    Done

--- frequencies.py
+++ frequencies.py
@@ -5,8 +5,5 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for i in range(len(items)):
-        if items[i] in frequencies:
             frequencies[items[i]] = frequencies[items[i]] + 1
-        else:
-            frequencies.update({items[i]: 1})
     return frequencies
